c6f5073e08a742cb9bd95405d72b21d4fa465b93
VBeal <victorbeal@gmail.com>
2025-01-03 01:00:17 -0300

    Atualizado searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,20 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { BsSearch } from "react-icons/bs";
+
 import "./SearchBar.css";
+import fetchProducts from "../../api/fetchProducts";
+import AppContext from "../../context/AppContext";
 
 function SearchBar() {
+  const { setProducts, setLoading } = useContext(AppContext);
   const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    const products = await fetchProducts(searchValue);
+
+    setProducts(products);
+    setLoading(false);
+    setSearchValue("");
+  };
+
   return (
-    <form className="search_bar">
+    <form className="search-bar" onSubmit={handleSearch}>
+      {name}
       <input
         type="search"
         value={searchValue}
-        placeholder="Buscar Produtos"
-        className="search_input"
+        placeholder="Buscar produtos"
+        className="search__input"
         onChange={({ target }) => setSearchValue(target.value)}
         required
       />
-      <button type="submit" className="search_button">
+
+      <button type="submit" className="search__button">
         <BsSearch />
       </button>
     </form>
